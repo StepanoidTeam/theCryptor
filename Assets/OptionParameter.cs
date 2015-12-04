f6cf50d29ae4ed9ef0c0ec1f6e60960a32cf89c6
bobanko <mishko.vladimir@gmail.com>
2015-12-04 21:43:51 +0200

    option parameter prefab in progress

---
+++ Assets/OptionParameter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionParameter : MonoBehaviour
+{
+    public string Text;
+    public string ParameterName;
+
+    UnityEngine.UI.Text paramNameTextbox;
+    UnityEngine.UI.Text paramValueTextbox;
+    UnityEngine.UI.Slider paramValueSlider;
+
+    
+
+    void Awake() {
+       
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetParameter()
+    {
+
+        //PlayerPrefs.SetInt("LastLevel", 1);
+        //ShowProgress();
+    }
+
+    public void GetParameter()
+    {
+        //ProgressText.text = PlayerPrefs.GetInt("LastLevel", 1).ToString();
+    }
+}
