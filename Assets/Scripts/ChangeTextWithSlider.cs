b84ba9d8b42d96e87091512626fdcefb9a33b6a7
stepastepa <stepanisworking@gmail.com>
2015-12-09 02:46:51 +0200

    Options Upgrade - the sliders are connected to their text labels

---
+++ Assets/Scripts/ChangeTextWithSlider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeTextWithSlider : MonoBehaviour {
+
+    public Text textLabel;
+    public Slider optionsSlider;
+
+    void Start() {
+        SetTextLabel();
+    }
+
+    public void SetTextLabel() {
+        textLabel.text = optionsSlider.value.ToString();
+    }
+}
